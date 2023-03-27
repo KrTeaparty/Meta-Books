7366a0d3b72a75a12abe6d2fdc98e400fee9b35e
PERSONN <sonth2405@naver.com>
2023-03-27 14:47:54 +0900

    add diary

---
+++ metabooks/diary/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+import requests
+import json
+
+
+def test_clova(API_KEYS,content):
+  url = "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze"
+
+  headers = {
+    "X-NCP-APIGW-API-KEY-ID": API_KEYS['clova_client_id'],
+    "X-NCP-APIGW-API-KEY": API_KEYS['clova_client_secret'],
+    "Content-Type": "application/json"
+  }
+  
+  data = {
+    "content": content
+  }
+
+  response = requests.post(url, data=json.dumps(data), headers=headers)
+  rescode = response.status_code
+  if (rescode == 200):
+    result = json.loads(response.text)
+    
+  else:
+    result = "Error :"+response.text
+    
+  return result
+
+
+
+
+def index(request):
+  template = loader.get_template('index.html')
+  content = request.GET.get('diary_txt')
+  
+  context = {}
+  if(content):
+    with open('./diary/API_KEYS.json', 'r') as f:
+      API_KEYS = json.load(f)
+  
+    result = test_clova(API_KEYS,content)
+
+    context = {
+      "txt_data": content,
+      "sentiment": result['document']['sentiment'],
+      "pos" : result['document']['confidence']['positive'],
+      "neu" : result['document']['confidence']['neutral'],
+      "neg" : result['document']['confidence']['negative'],
+    }
+  return HttpResponse(template.render(context, request))
