4eea140483132169b1d616d6ce34be5d3d08c11f
KrTeaparty <hjk125ha@naver.com>
2023-04-08 13:40:02 +0900

    <Refactor> Refactor code for save to DB

--- metabooks/bookreport/views.py
+++ metabooks/bookreport/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 import requests
 import json
 import openai
@@ -11,20 +10,23 @@ import datetime as dt
 class Book_report():
     def __init__(self, API_KEYS: dict):
         self.API_KEYS = API_KEYS
-        openai.api_key = API_KEYS['openai_api_key']
-        self.conn = sqlite3.connect('../db.sqlite3')
-        self.cur = self.conn.cursor()
         self.uid = 1
         self.content = ''
         self.keywords = ''
         self.image = None
         self.date = dt.datetime.now().strftime('%Y%m%d')
 
+    def insert_content(self, content):
+        self.content = content
+
     def test_openai(self):
+        print('===== test_openai 시작 =====')
+        openai.api_key = API_KEYS['openai_api_key']
         model = "gpt-3.5-turbo"
 
+        print('\t 1단계 시작')
         # 1. 독후감 묘사
-        query = "아래 독후감의 명장면을 묘사해주세요." + content
+        query = "아래 독후감의 명장면을 묘사해주세요." + self.content
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content":query}
@@ -36,6 +38,7 @@ class Book_report():
         ans = response['choices'][0]['message']['content']
         
         # 2. 답변 상세화
+        print('\t 2단계 시작')
         messages.append(
             {
                 "role": "assistant",
@@ -55,6 +58,7 @@ class Book_report():
         ans = response['choices'][0]['message']['content']
 
         # 3. 번역
+        print('\t 3단계 시작')
         messages = [
             {
                 "role": "system",
@@ -78,6 +82,7 @@ class Book_report():
         ans = response['choices'][0]['message']['content']
 
         # 4. 키워드 추출
+        print('\t 4단계 시작')
         messages = [
             {
                 "role": "system",
@@ -102,121 +107,70 @@ class Book_report():
         )
         # <수정 예정> DB에 추출한 키워드 저장 필요
         ans = response['choices'][0]['message']['content']
-        return ans
+        self.keywords = ans
+        print('\t 전체 단계 수행 완료: ', self.keywords)
 
+    def test_karlo(self):
+        print('===== test_karlo 시작 =====')
+        api = Karlo(service_key = self.API_KEYS['kakao_rest_api_key'])
 
+        prompt = ", concept art, 8K, ultra-detailed, illustration, Claude Monet"
+        prompt = f"{self.keywords}{prompt}"
 
+        img_dict = api.text_to_image(prompt, 1)
 
-def test_openai(API_KEYS: dict, content: str):
-  openai.api_key = API_KEYS['openai_api_key']
-  conn, cur = connect_db()
-  model = "gpt-3.5-turbo"
+        img_str = img_dict.get("images")[0].get('image')
 
-  # 1. 독후감 묘사
-  query = "아래 독후감의 명장면을 묘사해주세요." + content
-  messages = [
-      {"role": "system", "content": "You are a helpful assistant."},
-      {"role": "user", "content":query}
-  ]
-  response = openai.ChatCompletion.create(
-      model=model,
-      messages=messages,  # API에 보낼 텍스트
-  )
-  ans = response['choices'][0]['message']['content']
-  
-  # 2. 답변 상세화
-  messages.append(
-      {
-          "role": "assistant",
-          "content": ans
-      },
-  )
-  messages.append(
-      {
-          "role": "user",
-          "content": "위 내용을 바탕으로 전체적인 배경과 외형적인 모습을 더 자세히 상상해서 묘사해주세요."
-      }
-  )
-  response = openai.ChatCompletion.create(
-      model=model,
-      messages=messages
-  )
-  ans = response['choices'][0]['message']['content']
+        self.image = img_str
+        
+        print('\t 이미지 생성 완료')
 
-  # 3. 번역
-  messages = [
-      {
-          "role": "system",
-          "content": "You are a helpful assistant who is good at translating."
-      },
-      {
-          "role": "assistant",
-          "content": ans
-      }
-  ]
-  messages.append(
-      {
-          "role": "user",
-          "content": "영어로 번역해주세요."
-      }
-  )
-  response = openai.ChatCompletion.create(
-      model = model,
-      messages = messages
-  )
-  ans = response['choices'][0]['message']['content']
+    def insert_to_db(self):
+        print('==== DB에 삽입 시작 =====')
+        conn = sqlite3.connect('./metabooks/db.sqlite3')
+        cur = conn.cursor()
+        input_data = (self.uid, self.content, self.keywords, self.image, self.date)
+        sql = f'INSERT INTO book_report VALUES (?,?,?,?,?)'
+        cur.execute(sql, input_data)
+        conn.commit()
+        print('\t DB에 삽입 완료')
+        cur.close()
+        conn.close()
 
-  # 4. 키워드 추출
-  messages = [
-      {
-          "role": "system",
-          "content": "You are an assistant who is good at creating prompts for image creation."
-      },
-      {
-          "role": "assistant",
-          "content": ans
-      }
-  ]
-  # 사용자 메시지 추가
-  messages.append(
-      {
-          "role": "user", 
-          "content": "Condense up to 7 outward description to focus on nouns and adjectives separated by ','. Do not answer by each keyword."
-      }
-  )
-  # ChatGPT API 호출하기
-  response = openai.ChatCompletion.create(
-      model=model,
-      messages=messages
-  )
-  # <수정 예정> DB에 추출한 키워드 저장 필요
-  ans = response['choices'][0]['message']['content']
-  return ans
+'''
+사용법
+book = Book_report(API_KEYS)
 
-def test_karlo(API_KEYS: dict, content: str):
-  api = Karlo(service_key = API_KEYS['kakao_rest_api_key'])
-  prompt = ", concept art, 8K, ultra-detailed, illustration, Claude Monet"
-  prompt = f"{content}{prompt}"
+test_gpt_contents = """
+독후감 내용
+"""
 
-  img_dict = api.text_to_image(prompt, 1)
+book.insert_content(test_gpt_contents)
+book.test_openai()
+book.test_karlo()
+book.insert_to_db()
+'''
 
-  img_str = img_dict.get("images")[0].get('image')
+'''
+불러오는 법
+def get_image(img_str):
+    api = Karlo(service_key = API_KEYS['kakao_rest_api_key'])
+    image = api.string_to_image(base64_string = img_str, mode = 'RGBA')
+    image.save('./Features/test.png')
 
-  img = api.string_to_image(base64_string = img_str, mode = 'RGBA')
+conn = sqlite3.connect('./metabooks/db.sqlite3')
+cur = conn.cursor()
 
+sql = """SELECT * from book_report"""
+cur.execute(sql)
+record = cur.fetchall()
 
-  return img
-  # <수정 예정> 저장 경로 결정 필요
-  #img.save('./Features/test.png')
+for r in record:
+    get_image(r[3])
 
-def insert_content(UID, contents):
-  conn, cur = connect_db()
-  sql = 
+cur.close()
+conn.close()
+'''
 
 def index(request):
-  template = loader.get_template('bookreport/index.html')
-  
-  context={}
-  
-  
-  return HttpResponse(template.render(context,request))  
+  return HttpResponse("Bookreport")  
